22f2e9af8fb332309702da919fd6f734b4805805
levy002 <levy.ukwi002@gmail.com>
2022-08-09 18:11:16 +0200

    Configure databse

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+Greeting.create!([
+                   {
+                     message: 'Hello World!!',
+                     language: 'English'
+                   },
+                   {
+                     message: 'Bonjour le monde!!',
+                     language: 'French'
+                   },
+                   {
+                     message: 'Jambo Dunia!!',
+                     language: 'Swahili'
+                   },
+                   {
+                     message: 'Muraho neza!!',
+                     language: 'Kinyarwanda'
+                   },
+                   {
+                     message: 'Hola Mundo!!',
+                     language: 'Spanish'
+                   }
+                 ])
