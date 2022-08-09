8de39f624af944fb19dd390bb823c3ecc7fc3076
levy002 <levy.ukwi002@gmail.com>
2022-08-09 18:08:28 +0200

    Add api controllers

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @greeting = Greeting.all.sample
+        render json: @greeting
+      end
+    end
+  end
+end
